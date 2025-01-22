3d2b5e5ad7b4cc07201168a89d7ffb09530a4428
Jiho Park <ohijiho@gmail.com>
2025-01-23 08:57:32 +0900

    add http option

--- relay-server-nodejs/server.mjs
+++ relay-server-nodejs/server.mjs
@@ -1,37 +1,37 @@
-import https from 'https';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import * as uuid from 'uuid';
-import pino from 'pino';
-import { HostServer } from './host.mjs';
-import { RelayServer } from './relay.mjs';
-import { relayers, httpsOptions, httpsPort } from './configs.mjs';
+import https from "https";
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import * as uuid from "uuid";
+import pino from "pino";
+import { HostServer } from "./host.mjs";
+import { RelayServer } from "./relay.mjs";
+import * as configs from "./configs.mjs";
 
 const logger = pino();
 
-const IDENTITY_KEY = 'identity';
+const IDENTITY_KEY = "identity";
 
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.json({ type: "application/json" }));
 
-const hostServer = new HostServer(relayers, { logger });
+const hostServer = new HostServer(configs.relayers, { logger });
 const relayServer = new RelayServer(60000, { logger });
 
 const noLogPaths = new Set([
-  '/util.mjs',
-  '/stream.mjs',
-  '/crypto.mjs',
-  '/api.mjs',
+  "/util.mjs",
+  "/stream.mjs",
+  "/crypto.mjs",
+  "/api.mjs",
   // '/main.mjs',
-  '/vendor/qrcode.min.js',
-  '/vendor/StreamSaver.js',
-  '/favicon.ico',
+  "/vendor/qrcode.min.js",
+  "/vendor/StreamSaver.js",
+  "/favicon.ico",
 ]);
 
 app.use((req, res, next) => {
-  if (!noLogPaths.has(req.path) || req.url.includes('?'))
+  if (!noLogPaths.has(req.path) || req.url.includes("?"))
     logger.info(`${req.method} ${req.url}`);
   if (!req.cookies[IDENTITY_KEY]) {
     const identity = uuid.v4();
@@ -41,9 +41,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/host/open', (req, res) => {
+app.post("/api/host/open", (req, res) => {
   try {
-    const hid = hostServer.open(req.cookies[IDENTITY_KEY], new Date().getTime() + 60000);
+    const hid = hostServer.open(
+      req.cookies[IDENTITY_KEY],
+      new Date().getTime() + 60000,
+    );
 
     res.json({
       hid,
@@ -53,7 +56,7 @@ app.post('/api/host/open', (req, res) => {
   }
 });
 
-app.post('/api/host/close/:hid', (req, res) => {
+app.post("/api/host/close/:hid", (req, res) => {
   try {
     const hid = hostServer.close(req.params.hid, req.cookies[IDENTITY_KEY]);
 
@@ -63,11 +66,14 @@ app.post('/api/host/close/:hid', (req, res) => {
   } catch (e) {
     handleHostServerError(e, res);
   }
-})
+});
 
-app.post('/api/host/accept/:hid', async (req, res) => {
+app.post("/api/host/accept/:hid", async (req, res) => {
   try {
-    const d = await hostServer.accept(req.params.hid, req.cookies[IDENTITY_KEY]);
+    const d = await hostServer.accept(
+      req.params.hid,
+      req.cookies[IDENTITY_KEY],
+    );
     if (d) {
       res.json({
         ok: true,
@@ -84,7 +90,7 @@ app.post('/api/host/accept/:hid', async (req, res) => {
   }
 });
 
-app.post('/api/host/connect/:hid', (req, res) => {
+app.post("/api/host/connect/:hid", (req, res) => {
   try {
     const location = hostServer.connect(req.params.hid, req.body.message);
 
@@ -96,7 +102,7 @@ app.post('/api/host/connect/:hid', (req, res) => {
   }
 });
 
-app.post('/api/host/connect', (_, res) => {
+app.post("/api/host/connect", (_, res) => {
   try {
     const location = hostServer.genLocation();
 
@@ -108,7 +114,7 @@ app.post('/api/host/connect', (_, res) => {
   }
 });
 
-app.post('/api/relay/:sid/open', (req, res) => {
+app.post("/api/relay/:sid/open", (req, res) => {
   try {
     relayServer.open(req.params.sid, req.cookies[IDENTITY_KEY]);
     res.json({});
@@ -117,7 +123,7 @@ app.post('/api/relay/:sid/open', (req, res) => {
   }
 });
 
-app.post('/api/relay/:sid/close', (req, res) => {
+app.post("/api/relay/:sid/close", (req, res) => {
   try {
     relayServer.close(req.params.sid, req.cookies[IDENTITY_KEY]);
     res.json({});
@@ -126,12 +132,12 @@ app.post('/api/relay/:sid/close', (req, res) => {
   }
 });
 
-app.post('/api/relay/:sid/send', async (req, res) => {
-  if (!req.is('application/octet-stream')) {
+app.post("/api/relay/:sid/send", async (req, res) => {
+  if (!req.is("application/octet-stream")) {
     res.sendStatus(415);
     return;
   }
-  res.set('Content-Type', 'application/octet-stream');
+  res.set("Content-Type", "application/octet-stream");
   try {
     await relayServer.send(req.params.sid, req.cookies[IDENTITY_KEY], req);
     res.end();
@@ -140,8 +146,8 @@ app.post('/api/relay/:sid/send', async (req, res) => {
   }
 });
 
-app.post('/api/relay/:sid/recv', async (req, res) => {
-  res.set('Content-Type', 'application/octet-stream');
+app.post("/api/relay/:sid/recv", async (req, res) => {
+  res.set("Content-Type", "application/octet-stream");
   try {
     await relayServer.recv(req.params.sid, req.cookies[IDENTITY_KEY], res);
     res.end();
@@ -150,20 +156,24 @@ app.post('/api/relay/:sid/recv', async (req, res) => {
   }
 });
 
-app.get('/api/relay/:sid/closed', (req, res) => {
+app.get("/api/relay/:sid/closed", (req, res) => {
   try {
-    const closed = relayServer.isClosed(req.params.sid, req.cookies[IDENTITY_KEY], res);
+    const closed = relayServer.isClosed(
+      req.params.sid,
+      req.cookies[IDENTITY_KEY],
+      res,
+    );
     res.json({
       closed,
     });
   } catch (e) {
     handleRelayServerError(e, res);
   }
-})
+});
 
 function handleHostServerError(e, res) {
   if (res.headersSent) {
-    logger.error('error after headers sent', e);
+    logger.error("error after headers sent", e);
     return;
   }
   let status = 500;
@@ -177,7 +187,7 @@ function handleHostServerError(e, res) {
 
 function handleRelayServerError(e, res) {
   if (res.headersSent) {
-    logger.error('error after headers sent', e);
+    logger.error("error after headers sent", e);
     return;
   }
   let status = 500;
@@ -192,36 +202,66 @@ function handleRelayServerError(e, res) {
   res.sendStatus(status);
 }
 
-app.use(express.static('../static'));
-app.get('/favicon.ico', (_, res) => {
+app.use(express.static("../static"));
+app.get("/favicon.ico", (_, res) => {
   res.end();
 });
 
 function joinAddressAndPort(address, port) {
-  if (address.includes(':')) return `[${address}]:${port}`;
+  if (address.includes(":")) return `[${address}]:${port}`;
   return `${address}:${port}`;
 }
 
-https.createServer(httpsOptions, app).listen(httpsPort, () => {
-  logger.info(`listening at port ${httpsPort}`);
-}).on('connection', (socket) => {
-  logger.info(`Connection established: ${socket.remoteFamily} ${joinAddressAndPort(socket.remoteAddress, socket.remotePort)}`);
-});
+if (
+  !("httpPort" in configs) &&
+  !("httpsPort" in configs && "httpsOptions" in configs)
+) {
+  throw new Error("neither http nor https is enabled in config");
+}
+
+if ("httpPort" in configs) {
+  app.listen(configs.httpPort, () => {
+    logger.info(`listening for HTTP at port ${configs.httpPort}`);
+  });
+}
+
+if ("httpsPort" in configs && "httpsOptions" in configs) {
+  https
+    .createServer(configs.httpsOptions, app)
+    .listen(configs.httpsPort, () => {
+      logger.info(`listening for HTTPS at port ${configs.httpsPort}`);
+    })
+    .on("connection", (socket) => {
+      logger.info(
+        `Connection established: ${socket.remoteFamily} ${joinAddressAndPort(socket.remoteAddress, socket.remotePort)}`,
+      );
+    });
+}
 
 setInterval(() => {
   hostServer.prune();
   relayServer.prune();
 
-  if (hostServer.dbgHostsOpen || hostServer.dbgTotalListeners || hostServer.dbgSocksOpen || hostServer.dbgTotalUnfulfilled || hostServer.dbgTotalQueued) {
-    logger.info(`Garbage collection status:`)
+  if (
+    hostServer.dbgHostsOpen ||
+    hostServer.dbgTotalListeners ||
+    hostServer.dbgSocksOpen ||
+    hostServer.dbgTotalUnfulfilled ||
+    hostServer.dbgTotalQueued
+  ) {
+    logger.info(`Garbage collection status:`);
     if (hostServer.dbgHostsOpen)
       logger.info(` - ${hostServer.dbgHostsOpen} hosts are currently open`);
     if (hostServer.dbgTotalListeners)
-      logger.info(` - ${hostServer.dbgTotalListeners} listeners are currently waiting`);
+      logger.info(
+        ` - ${hostServer.dbgTotalListeners} listeners are currently waiting`,
+      );
     if (relayServer.dbgSocksOpen)
       logger.info(` - ${relayServer.dbgSocksOpen} sockets are currently open`);
     if (relayServer.dbgTotalUnfulfilled)
-      logger.info(` - ${relayServer.dbgTotalUnfulfilled} relay requests are unfulfilled`);
+      logger.info(
+        ` - ${relayServer.dbgTotalUnfulfilled} relay requests are unfulfilled`,
+      );
     if (relayServer.dbgTotalQueued)
       logger.info(` - ${relayServer.dbgTotalQueued} relay requests are queued`);
   }
